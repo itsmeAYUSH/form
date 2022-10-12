1bb60cde9d5a30770c99d62181cfd517b8cc530e
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2022-10-12 13:36:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var btn = document.querySelector('.btn');
 btn.addEventListener('click', (e)=>{
     document.querySelector('.btn').style.color = "white";
     document.querySelector('.btn').style.background = "black";
-
 });
 
 function saveToLocalStorage(event){
@@ -11,12 +10,18 @@ function saveToLocalStorage(event){
     const name = event.target.username.value;
     const email = event.target.emailId.value;
 
-
     let user = {
         name: name,
         email: email,
-      };
-
-    localStorage.setItem('user',JSON.stringify(user));
-    
+      }
+      localStorage.setItem(user.name,JSON.stringify(user));
+      addUser(user);
 }
+
+
+function addUser(user1){
+    const parentNode = document.getElementById('listOfUsers');
+    const childHTML = `<li> ${user1.name} - ${user1.email} </li>`;
+    parentNode.innerHTML =  parentNode.innerHTML + childHTML;
+}
+
